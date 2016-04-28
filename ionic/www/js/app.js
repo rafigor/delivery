4dbce97dd62e8f91429d43335c91b3e5d33ec0d0
Rafael <rafigor@gmail.com>
2016-04-27 23:58:42 -0300

    ionic
    ionic

--- ionic/www/js/app.js
+++ ionic/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -25,8 +25,9 @@ angular.module('starter', ['ionic'])
 
 .config(function($stateProvider){
   $stateProvider.state('home', {
-    url: '/home',
-    templateUrl: 'templates/home.html'
+    url: '/home/:nome',
+    templateUrl: 'templates/home.html',
+    controller: 'HomeCtrl'
   }).state('main', {
     url: '/',
     templateUrl: 'templates/main.html'
