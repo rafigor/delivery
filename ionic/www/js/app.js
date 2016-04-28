fb5588cd4b0fa6fba441f60a0fffd570011f101b
Rafael <rafigor@gmail.com>
2016-04-27 21:40:05 -0300

    routes
    routes

--- ionic/www/js/app.js
+++ ionic/www/js/app.js
@@ -22,3 +22,13 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider){
+  $stateProvider.state('home', {
+    url: '/home',
+    templateUrl: 'templates/home.html'
+  }).state('main', {
+    url: '/',
+    templateUrl: 'templates/main.html'
+  })
+});
