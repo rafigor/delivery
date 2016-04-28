8a62d869cc298e385a6d83484b92c0827f122dd3
Rafael <rafigor@gmail.com>
2016-04-28 16:01:04 -0300

    criar usuário

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -65,6 +65,7 @@ class AuthController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'role' => 'admin'
         ]);
     }
 }
