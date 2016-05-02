1bf75951803fae2724294e2a6a110ea4c0128deb
Rafael <rafigor@gmail.com>
2016-05-02 19:56:34 -0300

    Listagem de Pedidos

--- ionic/www/js/controllers/client/order.js
+++ ionic/www/js/controllers/client/order.js
@@ -8,6 +8,15 @@ angular.module('starter.controllers')
         $scope.orders = [];
         Order.query({id: null,include:'items'}, function(data){
             $ionicLoading.hide();
+            angular.forEach(data.data, function(item) {
+                switch(item.status){
+                    case 0: item.statusName = 'Pendente'; break;
+                    case 1: item.statusName = 'A caminho'; break;
+                    case 2: item.statusName = 'Entregue'; break;
+                    case 3: item.statusName = 'Cancelado'; break;
+                    default: item.statusName = 'Pendente'; break;
+                }
+            });
             $scope.orders = data.data;
         }, function(dataError){
             $ionicLoading.hide();
